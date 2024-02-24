f8953758bbb75de45c2637495c6ffc8495f99a72
mxttbrunet <brunetm2@winthrop.edu>
2024-02-24 08:05:10 -0500

    fixed junk

---
+++ symbolTable.h
@@ -0,0 +1,57 @@
+#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define HASHSIZE 101
+
+typedef struct Var{
+    char* name; 
+    char* type; 
+    struct Var* nextEntry;
+} var_t;
+
+
+static var_t * hashTable[HASHSIZE];
+char* typeArray[2] = {"int", "char"};
+
+unsigned int hash(char* varName){
+    
+    unsigned int hashVal;
+    for (hashVal = 0; *varName != '\0'; *varName++ )
+        hashVal = *varName + 31 * hashVal; 
+    return hashVal % HASHSIZE;
+    
+}
+
+var_t * lookUp(char* varName){
+    
+    var_t *np; 
+    
+    for (np = hashTable[hash(varName)]; np!= NULL; np = np->nextEntry)
+        if(!strcmp(varName,np->name))
+            return np; 
+    return NULL;
+}
+
+var_t *install(char*name, char*type){
+    
+    var_t * np;
+    unsigned int hashVal;
+    
+    if((np = lookUp(name)) == NULL){
+        np = (var_t*) malloc(sizeof(*np));
+        if( np == NULL || (np->name = strdup(name))== NULL)
+            return NULL; 
+        hashVal = hash(name);
+        np->nextEntry = hashTable[hashVal]; 
+        hashTable[hashVal] = np; 
+        
+    }
+    else
+        free((void*) np->type);
+        
+    if((np->type = strdup(type)) == NULL)
+        return NULL; 
+    return np; 
+}
+
+
